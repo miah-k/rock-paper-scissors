1e2acaa6881f22e99f255ceae88a35965ba69d4b
miah-k <miahkirton2@gmail.com>
2024-08-27 09:01:58 -0400

    Create a function to get the Computer's hand Function returns rock,paper,or scissors at random

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice(){
+    let pickerValue = Math.floor(Math.random() * 10)%3; 
+    let choice;
+    if(pickerValue == 0){
+      choice = "rock";
+    }
+    else if(pickerValue == 1){
+      choice = "paper";
+    }
+    else{
+      choice = "scissors";
+    }
+  
+    return choice; 
+  }
+  console.log(getComputerChoice());
